75bcfd1849d50119f308d393fd0d250272137fc6
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2022-12-01 21:47:27 +0600

    Project Upload

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Owl Carousel
+$(document).ready(function () {
+  $(".all-services").owlCarousel({
+    loop: true,
+    margin: 2,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".all-testimonial").owlCarousel({
+    loop: true,
+    margin: 2,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
